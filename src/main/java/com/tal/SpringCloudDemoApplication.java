84136ab9a4579fb482606ec099f7690b1181dabc
zhangchengpeng <zhangchengpeng@100tal.com>
2020-04-20 11:57:39 +0800

    init project

---
+++ src/main/java/com/tal/SpringCloudDemoApplication.java
@@ -0,0 +1,17 @@
+/**
+ * Talcloud.com Inc.
+ * Copyright (c) 2018-2020 All Rights Reserved.
+ */
+
+package com.tal;
+
+/**
+ * <pre>
+ *    启动类
+ * </pre>
+ *
+ * @version $Id: SpringCloudDemoApplication.java, v1.0 2020/4/20 11:41 zcp Exp $
+ */
+
+public class SpringCloudDemoApplication {
+}
